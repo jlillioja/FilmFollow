040c5c3ee95dd9fcbdb0d14adb39f3a64b50c4cc
jlillioja <jlillioja@gmail.com>
2015-11-06 15:38:03 -0600

    Project init, added apiKey and gridVIew.

---
+++ app/src/main/java/io/github/jlillioja/project1/MainActivity.java
@@ -0,0 +1,15 @@
+package io.github.jlillioja.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String apiKey = "756fb8861239cda5843ea91252ae7eff";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
