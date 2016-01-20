7f1640734c0e76b8a8a553acd9f0e32ebdc0166e
Jacob Lillioja <jlillioja@gmail.com>
2016-01-20 15:58:58 -0600

    Trailers now work, albeit with ugly code. I'll leave cleanup until after I have the functionality.

--- app/src/main/java/io/github/jlillioja/project1/MovieDetailsActivity.java
+++ app/src/main/java/io/github/jlillioja/project1/MovieDetailsActivity.java
@@ -1,8 +1,11 @@
 package io.github.jlillioja.project1;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,7 +15,18 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    JSONObject movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +34,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_details);
 
         Intent intent = getIntent();
+
         try {
-            JSONObject movie = new JSONObject(intent.getStringExtra(getString(R.string.key_movie)));
+            movie = new JSONObject(intent.getStringExtra(getString(R.string.key_movie)));
 
             ImageView image = (ImageView) findViewById(R.id.poster_imageView);
             ImageAdapter.loadImage(image, movie, this);
@@ -47,7 +62,73 @@ public class MovieDetailsActivity extends AppCompatActivity {
     }
 
     public void launchTrailer(View view) {
-        Toast.makeText(getApplicationContext(), getString(R.string.not_implemented), Toast.LENGTH_SHORT).show();
+        new launchTrailerTask().execute();
+
+    }
+
+    private class launchTrailerTask extends AsyncTask<Void, Void, Void> {
+        private final String LOG_TAG = launchTrailerTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            //Find Trailer
+        /* Declare urlConnection outside try/catch block so it can be closed in finally. */
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                URL url = new URL(Uri.parse(getString(R.string.tmdb_movie_path)).buildUpon()
+                        .appendPath(movie.getString("id"))
+                        .appendPath("videos")
+                        .appendQueryParameter(getString(R.string.api_key_query), getApplicationContext().getString(R.string.api_key))
+                        .build().toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                StringBuilder inStringBuilder = new StringBuilder();
+                String line;
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                while (((line = reader.readLine()) != null)) {
+                    inStringBuilder.append(line);
+                }
+
+                String inString = inStringBuilder.toString();
+                Log.v(LOG_TAG, inString);
+
+                JSONObject videosJSON = new JSONObject(inString);
+                String videoKey = videosJSON.getJSONArray(getString(R.string.results_key)).getJSONObject(0).getString(getString(R.string.key_trailer));
+
+                Log.d(LOG_TAG, "videoKey: " + videoKey);
+
+                Uri videoPath = Uri.parse(getString(R.string.youtube_path)).buildUpon()
+                        .appendPath("watch")
+                        .appendQueryParameter("v", videoKey)
+                        .build();
+
+                Log.d(LOG_TAG, "videoPath: "+videoPath.toString());
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(videoPath);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+
+            } catch (MalformedURLException err) {
+                err.printStackTrace();
+            } catch (IOException err) {
+                err.printStackTrace();
+            } catch (JSONException err) {
+                err.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
+                return null;
+            }
+        }
     }
 
     public void onToggleStar (View view) {
---
+++ app/src/main/java/io/github/jlillioja/project1/tmdbWrapper.java
@@ -0,0 +1,7 @@
+package io.github.jlillioja.project1;
+
+/**
+ * Created by jlillioja on 1/20/2016.
+ */
+public class tmdbWrapper {
+}
