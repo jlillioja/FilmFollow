3f3edc8410b81b1e829c31bc17eee4e9d2aa1ee6
Jacob Lillioja <jlillioja@gmail.com>
2016-02-19 14:27:27 -0600

    Revert "Revert "I want to store favorites offline. I've decided to use Sugar ORM to to this. I've restructured a number of things for this to work - MainActivity now stores its movies in a list rather than a json object containing a json array containing the movies.""
    This reverts commit 637b4bacd48a43b0c27561cf30c9e783854618ae.

---
+++ app/src/main/java/io/github/jlillioja/project1/Favorite.java
@@ -0,0 +1,16 @@
+package io.github.jlillioja.project1;
+
+import com.orm.SugarRecord;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONObject;
+
+/**
+ * Created by jlillioja on 2/11/2016.
+ */
+public class Favorite extends SugarRecord {
+
+    JSONObject movie;
+    public Favorite(){}
+
+}
